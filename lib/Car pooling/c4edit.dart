1cd9248d275a6414288fbc152d661fa1120c1f7c
muhammed Tanseer <112170428+thanseer02@users.noreply.github.com>
2023-07-05 20:36:44 +0530

    offer_ride

--- lib/Car pooling/c4edit.dart
+++ lib/Car pooling/c4edit.dart
@@ -6,14 +6,14 @@ import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'connect_location.dart';
-class c3 extends StatefulWidget {
-  const c3({Key? key}) : super(key: key);
-
+class c4edit extends StatefulWidget {
+   c4edit({Key? key,required this.starting_point,required this.destination,required this.time,required this.date,required this.vehicle_no,required this.pool_id}) : super(key: key);
+  var starting_point,destination,time,date,vehicle_no,pool_id;
   @override
-  State<c3> createState() => _c3State();
+  State<c4edit> createState() => _c4editState();
 }
 
-class _c3State extends State<c3> {
+class _c4editState extends State<c4edit> {
   var pick;
   var time_pick;
   var select=TimeOfDay.now();
@@ -22,83 +22,90 @@ class _c3State extends State<c3> {
   var vehicle_noctrl=TextEditingController();
   var datectrl=TextEditingController();
   var timectrl=TextEditingController();
-
+  var poolctrl=TextEditingController();
   Future<String?> getLoginId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? login_id = prefs.getString('loginId');
     return login_id;
   }
-  Future<Map<String?,String?>> getData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? log_Id = prefs.getString('log_id');
-    String? first_name = prefs.getString('first_name');
-    String? last_name = prefs.getString('last_name');
-    String? mobile_no = prefs.getString('mobile_no');
-    return {
-      'log_id': log_Id,
-      'first_name': first_name,
-      'last_name': last_name,
-      'mobile_no': mobile_no
-    };
+  @override
+  void initState(){
+    super.initState();
+    starting_pointctrl.text=widget.starting_point;
+    destinationctrl.text=widget.destination;
+    timectrl.text=widget.time;
+    datectrl.text=widget.date;
+    vehicle_noctrl.text=widget.vehicle_no;
+    print(widget.pool_id);
+
+    print(select);
+    getLoginId();
+  }
+  // Future<Map<String?,String?>> getData() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   String? log_Id = prefs.getString('log_id');
+  //   String? first_name = prefs.getString('first_name');
+  //   String? last_name = prefs.getString('last_name');
+  //   String? mobile_no = prefs.getString('mobile_no');
+  //   return {
+  //     'log_id': log_Id,
+  //     'first_name': first_name,
+  //     'last_name': last_name,
+  //     'mobile_no': mobile_no
+  //   };
+  // }
+
+  // void profile(first_name,last_name,log_id,mobile_no) async{
+  //     Map<String?,String?>data=await getData();
+  //     String? first_name=data['first_name'];
+  //     String? last_name=data['last_name'];
+  //     String? log_id=data['log_id'];
+  //     String? mobile_no=data['mobile_no'];
+  //
+  // }
+  Future<void> select_date() async {
+    final DateTime? pick= await showDatePicker(context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2100));
+    if (pick!=null && pick!=select_date) {
+      print(pick);
+      String formatteddate=DateFormat('yyyy-MM-dd').format(pick);
+      print(formatteddate);
+      datectrl.text=formatteddate ;
+
+      setState(() {
+        datectrl.text=formatteddate ;
+      });
+    }
+  }
+  Future<void> select_time() async {
+    final TimeOfDay?time_pick=await
+    showTimePicker(
+        context: context, initialTime: select);
+    if (time_pick!=null && time_pick!=select) {
+      print(time_pick);
+      String formattedtime=time_pick.format(context);
+      print(formattedtime);
+      setState(() {
+        // timectrl=time_pick;
+        select=time_pick;
+        timectrl.text=formattedtime ;
+
+      });
+    }
   }
 
-    // void profile(first_name,last_name,log_id,mobile_no) async{
-    //     Map<String?,String?>data=await getData();
-    //     String? first_name=data['first_name'];
-    //     String? last_name=data['last_name'];
-    //     String? log_id=data['log_id'];
-    //     String? mobile_no=data['mobile_no'];
-    //
-    // }
-Future<void> select_date() async {
-  final DateTime? pick= await showDatePicker(context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2100));
-  if (pick!=null && pick!=select_date) {
-    print(pick);
-    String formatteddate=DateFormat('yyyy-MM-dd').format(pick);
-    print(formatteddate);
-    datectrl.text=formatteddate ;
-
-    setState(() {
-      datectrl.text=formatteddate ;
-    });
-  }  
-}
-Future<void> select_time() async {
- final TimeOfDay?time_pick=await
- showTimePicker(
-     context: context, initialTime: select);
- if (time_pick!=null && time_pick!=select) {
-    print(time_pick);
-    String formattedtime=time_pick.format(context);
-    print(formattedtime);
-    setState(() {
-      // timectrl=time_pick;
-      select=time_pick;
-      timectrl.text=formattedtime ;
-
-    });
- }  
-}
-@override
-void initState(){
-  super.initState();
-  print(select);
-  getLoginId();
-
-}
   final offer_poolkey=GlobalKey<FormState>();
 
   Future<void> senddata()  async {
-  var log_id=await getLoginId();
-  if (log_id!=null) {
-    print("log_id:$log_id");
-  }
-  else
+    var log_id=await getLoginId();
+    if (log_id!=null) {
+      print("log_id:$log_id");
+    }
+    else
     {
-       print('error');
+      print('error');
 
     }
     var data={
@@ -107,13 +114,14 @@ void initState(){
       'vehicle_no':vehicle_noctrl.text,
       'date':datectrl.text,
       'time':timectrl.text,
-      'log_id':log_id
+      'log_id':log_id,
+      'pool_id':widget.pool_id
     };
-    var response=await post(Uri.parse("${con.url}offer_pool/offer_pool.php"),body: data);
+    var response=await post(Uri.parse("${con.url}offer_ride_menu/update.php"),body: data);
     print(response.body);
     if (response.statusCode==200) {
 
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Offer pool registerd')));
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Updated')));
       Navigator.pushAndRemoveUntil(context,MaterialPageRoute(builder: (context)=>c4()),(Route <dynamic> route )=>false);
     }
 
@@ -125,7 +133,7 @@ void initState(){
         child: SingleChildScrollView(
           child: Form(
             key: offer_poolkey,
-            child: Column( 
+            child: Column(
               children: [
                 Row(
                   children: [
@@ -231,7 +239,7 @@ void initState(){
                     controller: timectrl,
                     onTap: (){
 
-                        select_time();
+                      select_time();
 
 
                     },
@@ -295,10 +303,11 @@ void initState(){
                           if (offer_poolkey.currentState!.validate())
                           {
                             // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Registerd')));
+
                             senddata();
 
-                              }
-                        }, child: Text('ADD',style: TextStyle(fontSize: 20),)),
+                          }
+                        }, child: Text('Update',style: TextStyle(fontSize: 20),)),
                   ),
                 ),
 
