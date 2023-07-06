71c67e2c3f784c7e7ea47a02dbe733bf7f4a2971
muhammed Tanseer <112170428+thanseer02@users.noreply.github.com>
2023-07-06 14:19:38 +0530

    group trip,goods movement

--- goods_movement/read_goods.php
+++ goods_movement/read_goods.php
@@ -1,7 +1,7 @@
 <?php
 include '../connect.php';
-$log_id=$_POST['log_id'];
-$sql=mysqli_query($con,"SELECT * from goods_movement where log_id='$log_id'");
+// $log_id=$_POST['log_id'];
+$sql=mysqli_query($con,"SELECT * from goods_movement");
 $list=array();
 if($sql->num_rows>0)
 {
@@ -9,14 +9,13 @@ if($sql->num_rows>0)
     {
         $myarray['result']='success';
         $myarray['gm_id']=$row['gm_id'];
-        $myarray['log_id']=$row['log_id'];
+        // $myarray['log_id']=$row['log_id'];
         $myarray['starting_point']=$row['starting_point'];
         $myarray['destination']=$row['destination'];
         $myarray['vehicle_no']=$row['vehicle_no'];
         $myarray['date']=$row['date'];
         $myarray['time']=$row['time'];
-        array_push($list,$myarray);
-        
+        array_push($list,$myarray);    
     }
 }
 else
--- group_trip/view.php
+++ group_trip/view.php
@@ -1,22 +1,25 @@
 <?php
 include '../connect.php';
-$log_id=$_POST['log_id'];
-$sql=mysqli_query($con,"SELECT * from goods_movement where log_id='$log_id'");
+// $log_id=$_POST['log_id'];
+$sql=mysqli_query($con,"SELECT * from group_trip");
 $list=array();
 if($sql->num_rows>0)
 {
     while($row=mysqli_fetch_assoc($sql))
     {
         $myarray['result']='success';
-        $myarray['gm_id']=$row['gm_id'];
+        $myarray['gt_id']=$row['gt_id'];
         $myarray['log_id']=$row['log_id'];
+        $myarray['first_name']=$row['first_name'];
+        $myarray['last_name']=$row['last_name'];  
+        $myarray['mobile_no']=$row['mobile_no'];
         $myarray['starting_point']=$row['starting_point'];
         $myarray['destination']=$row['destination'];
         $myarray['vehicle_no']=$row['vehicle_no'];
+        $myarray['vehicle_type']=$row['vehicle_type'];
+        $myarray['group_name']=$row['group_name'];
         $myarray['date']=$row['date'];
-        $myarray['time']=$row['time'];
-        array_push($list,$myarray);
-        
+        array_push($list,$myarray);    
     }
 }
 else
